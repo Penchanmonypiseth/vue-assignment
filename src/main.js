a97a14747fed6dcad44fe9e13d1def1435a35db5
Pen Chanmonypiseth <penchanmonypisethGithub@gmail.com>
2022-12-22 12:14:33 +0700

    style bug

--- src/main.js
+++ src/main.js
@@ -4,5 +4,3 @@ import router from './router';
 import './assets/Global.css';
 import i18n from './i18n'
 createApp(App).use(i18n).use(router).mount('#app');
-
-console.log(process.env.VUE_APP_TEST);
